896fada65325b13e33b83bc98fa9cfb701baad3d
John Atef <johnatef60@yahoo.com>
2023-07-16 23:20:42 +0300

    3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -12,8 +12,10 @@ else:
     last_digit = -int(last_digit_str)
 
 if last_digit > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, last_digit))
 elif last_digit == 0:
     print("Last digit of {:d} is 0 and is 0".format(number))
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, last_digit))
