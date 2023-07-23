cf9be15eb9ed4e3085d658667e104191daa42cc1
John Atef <johnatef60@yahoo.com>
2023-07-24 00:55:15 +0300

    1. How to make a script dynamic!

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def num_of_args(*args):
+    return len(args)
+if num_of_args == 0:
+    print("0 arguments.")
+else:
+    print("{} argument:".format(num_of_args))
+    if argv.index(args) <= num_of_args:
+        print("{}: {}".format(index, arg))
+
+if __name__ == "__main__":
+    import sys
