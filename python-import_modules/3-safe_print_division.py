dc7403f1253d412961a67691c72e46c524ac77a3
John Atef <johnatef60@yahoo.com>
2023-07-26 23:35:17 +0300

    3. Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print(None)
+        print("None")
     else:
         print("Inside result: {}".format(result))
     finally:
