1e8eaaf24db3ef55d99612f646faf652acc4c305
John Atef <johnatef60@yahoo.com>
2023-07-27 23:25:41 +0300

    1. Present in both

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     for x in set_1:
-        for y in set_2:
-            if x == y:
-                return x
-            else:
-                return []
+        if x in set_2:
+            return x
+        else:
+            return []
