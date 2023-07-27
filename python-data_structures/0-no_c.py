e987b303031888946dbd3560ad4515d732fa088f
John Atef <johnatef60@yahoo.com>
2023-07-27 20:26:41 +0300

    0. Can you C me now?

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    lst = my_string.strip("c")
-    print(lst)
+    lst = my_string.translate({ord(i): None for i in ("c", "C")})
+    return lst
+
+print(no_c("school"))
