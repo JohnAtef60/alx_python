ce83a3dfebe65485245beb876b55e1efcd72fa47
John Atef <johnatef60@yahoo.com>
2023-08-12 00:50:01 +0300

    0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """square"""
 class square:
-    """
+"""
 square doing nothing
 """
     def __init__(self, size):
-        """
+    """
     Attributes:
     __size: is private
     """
