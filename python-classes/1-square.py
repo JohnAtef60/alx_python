68d8bc43891fb7001c8fb621dc22c0a16941e832
John Atef <johnatef60@yahoo.com>
2023-08-12 00:47:21 +0300

    1. Size validation

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""square"""
+class square:
+    """
+    square doing nothing
+    """
+    def __init__(self, size):
+        """
+        Attributes:
+        __size: is private
+        """
+        self.__size = size
